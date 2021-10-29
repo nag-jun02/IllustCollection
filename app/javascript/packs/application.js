c976b94ead7f280d2b1946785263e59cdbd06a84
nagatajunichi <junichi2002.n@gmail.com>
2021-10-29 14:35:05 +0900

    js_assets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,13 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require jquery3
+//= require popper
+//= require bootstrap
 //= require_tree .
 
-
+import 'bootstrap';
+import '../stylesheets/application';
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,25 @@
 const { environment } = require('@rails/webpacker')
-// 以下追記
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+
+// Vue.js フル版（Compiler入り）
+environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
+
+// ここから
+// jQueryとBootstapのJSを使えるように
 const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
-    })
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
 )
 // ここまで
+
+
 module.exports = environment
