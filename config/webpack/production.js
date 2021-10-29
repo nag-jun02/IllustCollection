3832e101664594748dd7397b4a138d05a2a37ebb
nagatajunichi <junichi2002.n@gmail.com>
2021-10-30 08:08:01 +0900

    js

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -5,19 +5,19 @@ const environment = require('./environment')
 module.exports = environment.toWebpackConfig()
 
 // Import the functions you need from the SDKs you need
-require (initializeApp) from "firebase/app";
+// require (initializeApp) from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAd8PslkOP7Hkn_eT19WLlesqoVgVPpbEQ",
-  authDomain: "illustcollection.firebaseapp.com",
-  projectId: "illustcollection",
-  storageBucket: "illustcollection.appspot.com",
-  messagingSenderId: "910772806006",
-  appId: "1:910772806006:web:78a4069206918d45b0da57"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAd8PslkOP7Hkn_eT19WLlesqoVgVPpbEQ",
+//   authDomain: "illustcollection.firebaseapp.com",
+//   projectId: "illustcollection",
+//   storageBucket: "illustcollection.appspot.com",
+//   messagingSenderId: "910772806006",
+//   appId: "1:910772806006:web:78a4069206918d45b0da57"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
