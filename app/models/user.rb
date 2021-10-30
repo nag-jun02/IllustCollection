a76d85eae1888d0c366e5069b1aaf9d204db6f45
nagatajunichi <junichi2002.n@gmail.com>
2021-10-30 15:10:41 +0900

    db

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :illusts, dependent: :destroy
   
-  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship",  dependent: :destroy
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
   has_many :following, through: :following_relationships
   has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
   has_many :followers, through: :follower_relationships
--- db/migrate/20211015084223_create_relationships.rb
+++ db/migrate/20211015084223_create_relationships.rb
@@ -2,7 +2,7 @@ class CreateRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :relationships do |t|
       t.integer :follower_id
-      t.integer :folloing_id
+      t.integer :following_id
 
       t.timestamps
       t.index [:follower_id, :following_id], unique: true
