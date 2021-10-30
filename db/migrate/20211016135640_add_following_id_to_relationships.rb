8b43ad92aac6c775dd5c3a04c4d443ac48631efd
nagatajunichi <junichi2002.n@gmail.com>
2021-10-30 17:05:23 +0900

    edit

--- db/migrate/20211016135640_add_following_id_to_relationships.rb
+++
@@ -1,5 +0,0 @@
-class AddFollowingIdToRelationships < ActiveRecord::Migration[6.1]
-  def change
-    add_column :relationships, :following_id, :integer
-  end
-end
--- db/migrate/20211030054028_remove_followed_id_from_relationships.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFollowedIdFromRelationships < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :relationships, :followed_id, :integer
-  end
-end
